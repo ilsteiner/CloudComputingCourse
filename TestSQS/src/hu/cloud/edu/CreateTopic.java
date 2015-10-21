ef04f3c7d4a12467317523db90cc45c7a45681be
ilsteiner <izakizak@gmail.com>
2015-10-21 15:13:01 -0400

    Finished problem 2

--- TestSQS/src/hu/cloud/edu/CreateTopic.java
+++ TestSQS/src/hu/cloud/edu/CreateTopic.java
@@ -1,5 +1,6 @@
 package hu.cloud.edu;
 
+import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Map;
 import com.amazonaws.AmazonClientException;
@@ -11,6 +12,7 @@ import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.CreateTopicRequest;
 import com.amazonaws.services.sns.model.GetTopicAttributesRequest;
 import com.amazonaws.services.sns.model.GetTopicAttributesResult;
+import com.amazonaws.services.sns.model.SetTopicAttributesRequest;
 import com.amazonaws.services.sns.model.SubscribeRequest;
 import com.amazonaws.services.sns.model.SubscribeResult;
 import com.amazonaws.services.sns.model.Topic;
@@ -18,42 +20,39 @@ import com.amazonaws.services.sns.model.Topic;
 public class CreateTopic {
 
 	public static void main(String[] args) throws Exception {
-        AWSCredentials credentials = null;
-        try {
-            credentials = new ProfileCredentialsProvider("ZoranJavaDSDK").getCredentials();
-        } catch (Exception e) {
-            throw new AmazonClientException(
-                    "Cannot load the credentials from the credential profiles file. " +
-                    "Please make sure that your credentials file is at the correct " +
-                    "location (C:\\Users\\073621\\.aws\\credentials), and is in valid format.",
-                    e);
-        }
-
-        AmazonSNS sns = new AmazonSNSClient(credentials);
+        AmazonSNS sns = new AmazonSNSClient();
 		try {
 			// Create a Topic
-			System.out.println("Creating MyTopic11.\n");
-			CreateTopicRequest createTopicRequest = new CreateTopicRequest().withName("MyTopic11");
+			System.out.println("Creating MyTopic.\n");
+			CreateTopicRequest createTopicRequest = new CreateTopicRequest().withName("MyTopic");
 			
 			// Retrieve Amazon Resource Name
 			String myTopicArn = sns.createTopic(createTopicRequest).getTopicArn();
 			System.out.println("Topic created: " + myTopicArn);
 			
 			Thread.sleep(1000);
+			
+			sns.setTopicAttributes(new SetTopicAttributesRequest(myTopicArn,"DisplayName","TestTopic"));
+			
 			// List Topics
 			System.out.println("List topics: ");
 			for (Topic topic : sns.listTopics().getTopics()) {
 				System.out.println(" TopicArn: " + topic.getTopicArn());
 			}
-//			String myTopicArn = "arn:aws:sns:us-east-1:632259447628:MyTopic11";
 			
-			// Subscribe zdjordj@fas.harward.edu to MyTopic1
-			subscribe(sns, myTopicArn, "zdjordj@fas.harvard.edu");
-			subscribe(sns, myTopicArn, "zdjordje123@yahoo.com");
+			// Subscribe email to topic
+			//subscribe(sns, myTopicArn, "ilebwohlsteiner@gmail.com");
+			//subscribe(sns, myTopicArn, "16314069666");
 
 			// Fetch Topic attributes
 			System.out.println("Topic attributes: ");
 			fetchAttr(sns, myTopicArn);
+			
+			//Send a message
+			System.out.println("Sending message with id " + sns.publish(myTopicArn, "This is the message that I'm sending via both SMTP and SMS!").getMessageId());
+			
+			//Print subscription arns
+			System.out.println(sns.listSubscriptions().toString());
 			
 			// Delete a Topic
 			// System.out.println("Deleting the test Topic.\n");
@@ -68,11 +67,26 @@ public class CreateTopic {
 			System.out.println("Error Message: " + ace.getMessage());
 		}
 	}
-	private static void subscribe(AmazonSNS sns, String myTopicArn, String email) {
-		SubscribeRequest subReq = new SubscribeRequest(myTopicArn, "email", email);
+	private static void subscribe(AmazonSNS sns, String myTopicArn, String dest) {
+		String numRegex = "[0-9]+";
+		String emailRegex = ".+@.+";
+		
+		SubscribeRequest subReq;
+		
+		if(dest.matches(numRegex)){
+			subReq = new SubscribeRequest(myTopicArn, "sms", dest);
+		}
+		
+		else if(dest.matches(emailRegex)){
+			subReq = new SubscribeRequest(myTopicArn, "email", dest);
+		}
+		else{
+			throw new InvalidParameterException();
+		}
+		
 		SubscribeResult subRes = sns.subscribe(subReq);
 		String subscribedTopicArn = subRes.getSubscriptionArn();
-		System.out.println("Subscribed " + email + " to topic: " + subscribedTopicArn);
+		System.out.println("Subscribed " + dest + " to topic: " + subscribedTopicArn);
 	}
 
 	private static void fetchAttr(AmazonSNS sns, String myTopicArn) {
