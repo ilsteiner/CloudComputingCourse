396936ee5bc097c601ccfb30bb1be0125e906f47
ilsteiner <izakizak@gmail.com>
2015-11-10 13:43:50 -0500

    Homework 10

---
+++ hw10/Duplicator/src/hw10/hu/edu/LambdaFunctionHandler.java
@@ -0,0 +1,14 @@
+package hw10.hu.edu;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<String,String> {
+
+    @Override
+    public String handleRequest(String input, Context context) {
+        context.getLogger().log(input + input);
+        
+		return input+input;        
+    }
+}
